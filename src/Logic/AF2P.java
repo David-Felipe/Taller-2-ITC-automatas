dcbbbc252ee3331db515754273f8e9d3f3238425
Khokhlakov <86792425+Khokhlakov@users.noreply.github.com>
2023-06-18 22:14:29 -0500

    Clase AF2P lista menos lectura y creacion de archivos .msm

--- src/Logic/AF2P.java
+++ src/Logic/AF2P.java
@@ -1,9 +1,5 @@
 package Logic;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.time.*;
@@ -172,29 +168,29 @@ public class AF2P {
 
             if (pila1.empty() && pila2.empty() && this.F.contains(estado)){
                 this.esAceptado = true;
-                str += ">>accepted";
-                this.procesamientosAceptacion.add(str);
-                this.procesamientos.add(str);
+                String finalStr = str + ">>accepted";
+                this.procesamientosAceptacion.add(finalStr);
+                this.procesamientos.add(finalStr);
                 if (this.numPasosProcesamientoAceptacion < 0) {
                     this.numPasosProcesamientoAceptacion = numPasos;
-                    this.procesamientoAceptacion = str;
+                    this.procesamientoAceptacion = finalStr;
                 } else {
                     if (this.numPasosProcesamientoAceptacion > numPasos) {
                         this.numPasosProcesamientoAceptacion = numPasos;
-                        this.procesamientoAceptacion = str;
+                        this.procesamientoAceptacion = finalStr;
                     }
                 }
             } else {
-                str += ">>rejected";
-                this.procesamientosRechazo.add(str);
-                this.procesamientos.add(str);
+                String finalStr = str + ">>rejected";
+                this.procesamientosRechazo.add(finalStr);
+                this.procesamientos.add(finalStr);
                 if (this.numPasosProcesamientoRechazo < 0) {
                     this.numPasosProcesamientoRechazo = numPasos;
-                    this.procesamientoRechazo = str;
+                    this.procesamientoRechazo = finalStr;
                 } else {
                     if (this.numPasosProcesamientoRechazo > numPasos) {
                         this.numPasosProcesamientoRechazo = numPasos;
-                        this.procesamientoRechazo = str;
+                        this.procesamientoRechazo = finalStr;
                     }
                 }
             }
@@ -301,16 +297,16 @@ public class AF2P {
 
             if (abortado){
                 this.numProcesamientos++;
-                str += ">>aborted";
-                this.procesamientosRechazo.add(str);
-                this.procesamientos.add(str);
+                String finalStr = str + ">>aborted";
+                this.procesamientosRechazo.add(finalStr);
+                this.procesamientos.add(finalStr);
                 if (this.numPasosProcesamientoRechazo < 0) {
                     this.numPasosProcesamientoRechazo = numPasos;
-                    this.procesamientoRechazo = str;
+                    this.procesamientoRechazo = finalStr;
                 } else {
                     if (this.numPasosProcesamientoRechazo > numPasos) {
                         this.numPasosProcesamientoRechazo = numPasos;
-                        this.procesamientoRechazo = str;
+                        this.procesamientoRechazo = finalStr;
                     }
                 }
             }
@@ -374,7 +370,12 @@ public class AF2P {
                             strPila1 = new StringBuilder(strPila1).reverse().toString();
                             String strPila2 = copiaPila2.toString().replaceAll(", |\\[|\\]","");
                             strPila2 = new StringBuilder(strPila2).reverse().toString();
-                            String newStr = str + "->(" + imagen[0] + "," + cadena + ",";
+                            String newStr = str + "->(" + imagen[0] + ",";
+                            if (cadena.isEmpty()) { 
+                                newStr += "$,";
+                            } else {
+                                newStr += cadena + ",";
+                            }
                             if (strPila1.isEmpty()) { 
                                 newStr += "$,";
                             } else {
@@ -424,4 +425,190 @@ public class AF2P {
         System.out.println(this.numProcesamientos);
 
     }
+
+    public Boolean procesarCadena(String cadena) {
+        this.preprocesarCadena(cadena);
+        return this.esAceptado;
+    }
+
+    public Boolean procesarCadenaConDetalles(String cadena) {
+        this.preprocesarCadena(cadena);
+        System.out.println("Cadena: " + cadena);
+        if (this.esAceptado){
+            
+            System.out.println(this.procesamientoAceptacion);
+        } else {
+            for (String procesamiento : this.procesamientosRechazo) {
+                System.out.println(procesamiento);
+            }
+        }
+        return this.esAceptado;
+    }
+
+    public int computarTodosLosProcesamientos(String cadena, String nombreArchivo) {
+        this.preprocesarCadena(cadena);
+
+        StringBuilder sbAceptacion = new StringBuilder();
+        StringBuilder sbRechazo = new StringBuilder();
+
+        // Imprimir cada uno de los posibles procesamientos
+        System.out.println("Procesamientos posibles para la cadena: " + cadena);
+        for (String procesamiento : this.procesamientos) {
+            System.out.println(procesamiento);
+        }
+        System.out.println();
+
+
+        for (String procesamientoAceptado : this.procesamientosAceptacion) {
+            sbAceptacion.append(procesamientoAceptado + "\n");
+        }
+        String aceptacion = sbAceptacion.toString();
+        System.out.println("Procesamientos de aceptacion de la cadena: " + cadena);
+        if (this.esAceptado){
+            System.out.println(aceptacion);
+        } else {
+            System.out.println("La cadena \"" + cadena + "\" no es aceptada.");
+        }
+
+
+        for (String procesamientoRechazado : this.procesamientosRechazo) {
+            sbRechazo.append(procesamientoRechazado + "\n");
+        }
+        String rechazo = sbRechazo.toString();
+        System.out.println("Procesamientos rechazados de la cadena: " + cadena);
+        if (rechazo.isBlank()){
+            System.out.println("todos los procesamientos de la cadena \"" + cadena + "\" son de aceptacion.");
+        } else {
+            System.out.println(rechazo);
+        }
+
+        String nombreArchivoAceptadas = nombreArchivo + "AceptadasAF2P.txt";
+        String nombreArchivoRechazadas = nombreArchivo + "RechazadasAF2P.txt";
+
+        // Crear los archivos y llenarlo con las listas de estados correspondientes
+        try {
+
+            // Crear el archivo de cadenas aceptadas
+            File archivoAceptadas = new File(nombreArchivoAceptadas);
+            archivoAceptadas.createNewFile();
+
+            // Escribir la lista en el archivo
+            FileWriter escritor = new FileWriter(archivoAceptadas);
+            escritor.write(aceptacion);
+            escritor.close();
+
+            System.out.println("Archivo de cadenas aceptadas creado exitosamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al crear el archivo de cadenas aceptadas.");
+
+        }
+        try {
+
+            // Crear el archivo de cadenas rechazadas
+            File archivoRechazadas = new File(nombreArchivoRechazadas);
+            archivoRechazadas.createNewFile();
+
+            // Escribir la lista en el archivo
+            FileWriter escritor = new FileWriter(archivoRechazadas);
+            escritor.write(rechazo);
+            escritor.close();
+
+            System.out.println("Archivo de cadenas rechazadas creado exitosamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al crear el archivo de cadenas rechazadas.");
+
+        }
+
+        // Retornar el numero total de procesamientos posibles
+        return this.numProcesamientos;
+    }
+
+    public void procesarListaCadenas(Iterable<String> listaCadenas, String nombreArchivo, Boolean imprimirPantalla) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String cadena : listaCadenas) {
+
+            this.preprocesarCadena(cadena);
+
+            String fueAceptada = "no";
+            String procesamientoMasCorto;
+            if (this.esAceptado) {
+                fueAceptada = "yes";
+                procesamientoMasCorto = this.procesamientoAceptacion;
+            } else {
+                procesamientoMasCorto = this.procesamientoRechazo;
+            }
+            sb.append(cadena + "\t"
+                    + procesamientoMasCorto + "\t"
+                    + this.numProcesamientos + "\t"
+                    + this.procesamientosAceptacion.size() + "\t"
+                    + this.procesamientosRechazo.size() + "\t"
+                    + fueAceptada + "\n");
+
+        }
+
+        String listaProcesada = sb.toString();
+
+        // Imprimir si imprimirPantalla
+        if (imprimirPantalla) {
+            System.out.println(listaProcesada);
+        }
+
+        // Comprobar que el nombre del archivo es valido
+        for (int i = 0; i < nombreArchivo.length(); i++) {
+            Character simbolo = nombreArchivo.charAt(i);
+            if (!Character.isDigit(simbolo) && !Character.isLetter(simbolo) && !(simbolo >= ',' && simbolo <= '.')
+                    && !(simbolo == '_')) {
+                nombreArchivo = "procesamiento-cadenas-AF2P_"
+                        + LocalDateTime.now().getYear() + "-"
+                        + LocalDateTime.now().getMonthValue() + "-"
+                        + LocalDateTime.now().getDayOfMonth() + "-"
+                        + LocalDateTime.now().getHour() + "-"
+                        + LocalDateTime.now().getMinute() + "-"
+                        + LocalDateTime.now().getSecond() + "_"
+                        + Integer.toString(this.hashCode());
+                break;
+            }
+        }
+        if (nombreArchivo.isBlank()){
+            nombreArchivo = "procesamiento-cadenas-AF2P_"
+                        + LocalDateTime.now().getYear() + "-"
+                        + LocalDateTime.now().getMonthValue() + "-"
+                        + LocalDateTime.now().getDayOfMonth() + "-"
+                        + LocalDateTime.now().getHour() + "-"
+                        + LocalDateTime.now().getMinute() + "-"
+                        + LocalDateTime.now().getSecond() + "_"
+                        + Integer.toString(this.hashCode());
+        }
+
+        // Crear el archivo y llenarlo con listaProcesada
+        nombreArchivo += ".txt";
+        try {
+
+            // Crear el archivo de procesamientos de cadenas
+            File archivo = new File(nombreArchivo);
+            archivo.createNewFile();
+
+            // Escribir la lista en el archivo
+            FileWriter escritor = new FileWriter(archivo);
+            escritor.write(listaProcesada);
+            escritor.close();
+
+            System.out.println("Archivo de procesamientos de las cadenas listadas creado exitosamente.");
+
+        } catch (IOException e) {
+
+            System.out.println("Error al crear el archivo de procesamientos de las cadenas listadas.");
+        }
+    }
+
+    public void procesarListaCadenas(String[] listaCadenas, String nombreArchivo, Boolean imprimirPantalla) {
+        this.procesarListaCadenas(Arrays.asList(listaCadenas), nombreArchivo, imprimirPantalla);
+    }
+
 }
